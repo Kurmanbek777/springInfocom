04700d81a6992deca412efa0f91c758fc0d2a007
Kurmanbek777 <knurkamil@gmail.com>
2017-08-23 17:14:28 +0000

    исправлены ошибки
    исправлены ошибки

--- springInfocom/src/main/java/net/proselyte/springsecurityapp/dao/RoleDao.java
+++ springInfocom/src/main/java/net/proselyte/springsecurityapp/dao/RoleDao.java
@@ -4,4 +4,6 @@ import net.proselyte.springsecurityapp.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleDao extends JpaRepository<Role, Long> {
+
+    public Role getOne(long l);
 }
